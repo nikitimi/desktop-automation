561b9158194108340c4e320efa3c30a76b328a91
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2025-05-23 17:12:57 +0800

    feat: experimental cottontails auto-capture.

--- src/main.py
+++ src/main.py
@@ -47,7 +47,12 @@ class MSLAutomationCore:
             "apex-battles": self.__getfilepath('apex-battles.png'),
             "aria-lake": self.__getfilepath('aria-lake.png'),
             "aurora-plateau": self.__getfilepath('aurora-plateau.png'),
+            "auto-battle": self.__getfilepath('auto-battle.png'),
             "back": self.__getfilepath('back.png'),
+            "capture-success": self.__getfilepath('capture-success.png'),
+            "catch": self.__getfilepath('catch.png'),
+            "clan-notice-exit-button": self.__getfilepath('clan-notice-exit-button.png'),
+            "cottontails": self.__getfilepath('cottontails.png'),
             "dispatch": self.__getfilepath('dispatch.png'),
             "dungeons": self.__getfilepath('dungeons.png'),
             "emulator-relative-reference": self.__getfilepath('emulator-relative-reference.png'),
@@ -59,14 +64,20 @@ class MSLAutomationCore:
             "lunar-valley": self.__getfilepath('lunar-valley.png'),
             "magma-crags": self.__getfilepath('magma-crags.png'),
             "mirage-ruins": self.__getfilepath('mirage-ruins.png'),
+            "no-mode-capture": self.__getfilepath('no-mode-capture.png'),
+            "normal-speed": self.__getfilepath('normal-speed.png'),
+            "normal-stage-final-round": self.__getfilepath('normal-stage-final-round.png'),
             "pagos-coast": self.__getfilepath('pagos-coast.png'),
             "phantom-forest": self.__getfilepath('phantom-forest.png'),
             "play": self.__getfilepath('play.png'),
+            "quick-restart": self.__getfilepath('quick-restart.png'),
             "seabed-caves": self.__getfilepath('seabed-caves.png'),
             "shady-shop": self.__getfilepath('shady-shop.png'),
             "skip-dialogue": self.__getfilepath('skip-dialogue.png'),
             "sky-falls": self.__getfilepath('sky-falls.png'),
             "slumbering-city": self.__getfilepath('slumbering-city.png'),
+            "stage-battle": self.__getfilepath('stage-battle.png'),
+            "stage-clear": self.__getfilepath('stage-clear.png'),
             "star-sanctuary": self.__getfilepath('star-sanctuary.png'),
             "tower-of-chaos": self.__getfilepath('tower-of-chaos.png'),
         }
@@ -93,6 +104,7 @@ class MSLAutomationCore:
             while True:
                 
                 self.navigateto('skip-dialogue', True)
+                self.navigateto('clan-notice-exit-button', True)
         except:
             print('Passing Skip dialouge error, no more skips found.')
             pass
@@ -116,39 +128,86 @@ def main():
     # mslac.navigateto('play')
     # mslac.skipdialouge()
 
-    first_region_stages = ('phantom-forest', 'lunar-valley', 'aria-lake', 'mirage-ruins')
-    second_region_stages = ('pagos-coast', 'seabed-caves', 'magma-crags', 'star-sanctuary')
-    third_region_stages = ('sky-falls', 'slumbering-city', 'glacial-plains', 'aurora-plateau')
+    # first_region_stages = ('phantom-forest', 'lunar-valley', 'aria-lake', 'mirage-ruins')
+    # second_region_stages = ('pagos-coast', 'seabed-caves', 'magma-crags', 'star-sanctuary')
+    # third_region_stages = ('sky-falls', 'slumbering-city', 'glacial-plains', 'aurora-plateau')
 
-    for stage in first_region_stages:
-        mslac.navigateto('play', event='double_click')
-        # mslac.skipdialouge()
-        mslac.navigateto(stage)
-        mslac.navigateto('exit-stages-details')
-        mslac.navigateto('back',image_confidence=0.7)
+    # for stage in first_region_stages:
+    #     mslac.navigateto('play', event='double_click')
+    #     mslac.skipdialouge()
+    #     mslac.navigateto(stage)
+    #     mslac.navigateto('exit-stages-details')
+    #     mslac.navigateto('back',image_confidence=0.7)
 
-    # [REGION NAVIGATION]
-    x, y = mslac.navigateto('emulator-relative-reference', event='locate')
+    # # [REGION NAVIGATION]
+    # x, y = mslac.navigateto('emulator-relative-reference', event='locate')
 
-    # [SECOND REGION]
-    for stage in second_region_stages:
-        mslac.navigateto('play', event='double_click')
-        # mslac.skipdialouge()
-        pyautogui.moveTo(x+600,y+200)
-        pyautogui.dragTo(x, None, duration=.5, tween=pyautogui.easeInOutQuad)
-        mslac.navigateto(stage)
-        mslac.navigateto('exit-stages-details')
-        mslac.navigateto('back',image_confidence=0.7)
+    # # [SECOND REGION]
+    # for stage in second_region_stages:
+    #     mslac.navigateto('play', event='double_click')
+    #     mslac.skipdialouge()
+    #     pyautogui.moveTo(x+600,y+200)
+    #     pyautogui.dragTo(x, None, duration=.5, tween=pyautogui.easeInOutQuad)
+    #     mslac.navigateto(stage)
+    #     mslac.navigateto('exit-stages-details')
+    #     mslac.navigateto('back',image_confidence=0.7)
 
-    # [THIRD REGION]
-    for stage in third_region_stages:
-        mslac.navigateto('play', event='double_click')
-        # mslac.skipdialouge()
-        pyautogui.moveTo(x+800,y+200)
-        pyautogui.dragTo(x-525, None, duration=.5, tween=pyautogui.easeInOutQuad)
-        mslac.navigateto(stage)
-        mslac.navigateto('exit-stages-details')
-        mslac.navigateto('back',image_confidence=0.7)
+    # # [THIRD REGION]
+    # for stage in third_region_stages:
+    #     mslac.navigateto('play', event='double_click')
+    #     mslac.skipdialouge()
+    #     pyautogui.moveTo(x+800,y+200)
+    #     pyautogui.dragTo(x-525, None, duration=.5, tween=pyautogui.easeInOutQuad)
+    #     mslac.navigateto(stage)
+    #     mslac.navigateto('exit-stages-details')
+    #     mslac.navigateto('back',image_confidence=0.7)
+
+    # [ARIA LAKE FARM]
+    # mslac.navigateto('play', event='double_click')
+    # mslac.skipdialouge()
+    mslac.navigateto('aria-lake')
+    mslac.navigateto('exp-bonus-stage')
+    mslac.navigateto('stage-battle')
+
+    # SET AUTO-BATTLE x4 SPEED.
+    # box = mslac.navigateto('normal-speed', timeoutInSeconds=10, event='locate')
+    # if box != None:
+    #     x,y = box
+    #     for _ in range(4):
+    #         pyautogui.click(x,y,interval=0.5)
+
+    stage_loop = True
+    # mslac.navigateto('auto-battle')
+
+    while stage_loop:
+        final_round = mslac.navigateto('normal-stage-final-round', timeoutInSeconds=10, event=
+                        'locate', image_confidence=1)
+        
+        while final_round != None:
+            mslac.navigateto('auto-battle')
+            capture_location = mslac.navigateto('catch', event='locate', timeoutInSeconds=10)
+
+            if capture_location == None:
+                mslac.navigateto('auto-battle')
+                break
+
+            pyautogui.click(capture_location[0],capture_location[1])
+            monster_location = mslac.navigateto('cottontails', timeoutInSeconds=5, event=
+                                'locate')
+            if monster_location != None:
+                x,y = monster_location
+                pyautogui.click(x,y, event='double_click')
+            mslac.navigateto('capture-success', timeoutInSeconds=10)
+        
+        stage_clear = mslac.navigateto('stage-clear', timeoutInSeconds=10, event='locate')
+        if stage_clear != None:
+            x,y = stage_clear
+            pyautogui.doubleClick(x,y, interval=1)
+            pyautogui.doubleClick(x,y, interval=2)
+            pyautogui.click(x,y)
+            mslac.navigateto('quick-restart')
+            stage_loop = False
+
 
 if __name__ == "__main__":
     main()
