88e2e56d8b2675189350165cfdb6046c0785adf1
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2025-05-23 09:54:23 +0800

    feat: stages navigation from first to third region.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,154 @@
 import pyautogui
+import os
 
-screenWidth, screenHeight = pyautogui.size()
+pyautogui.PAUSE = .5
 
-print('Width: %s\tHeight: %s' % (screenWidth, screenHeight))
+class MSLAutomationCore:
+    """
+        Automation for MSL in MuMu Player 12.
+    """
+    __images_path = ('src','images')
+    __dir:tuple
 
-x, y = pyautogui.locateCenterOnScreen('src/images/file-explorer.jpg', confidence=0.9)
-pyautogui.moveTo(x, y, duration=1, tween=pyautogui.easeInOutQuad)
+    def __init__(self):
+        """
+            Load files from `src/images` directory.
+        """
+        # file_paths = []
+        # for file_path in os.listdir(self.getrootpath()):
+        #     file_paths.append(self.__getfilepath__(file_path))
+        self.__dir = tuple(os.listdir(self.getrootpath()))
+
+    def getrootpath(self):
+        """
+            Transform immutable images path into a proper path.
+        """
+        return '/'.join(self.__images_path)
+    
+    def getfiles(self):
+        """
+            Returns the collected file names.
+        """
+        return self.__dir
+    
+    def navigateto(
+            self, 
+            filename:str, 
+            throw_error:bool=None, 
+            image_confidence:float=0.9,
+            timeoutInSeconds:int=4,
+            event:str='click'
+        ):
+        """
+            This will throw pyautogui.ImageNotFoundException in error.
+            event - `click` | `double_click` | `locate`
+        """
+        image_dictionary = {
+            "apex-battles": self.__getfilepath('apex-battles.png'),
+            "aria-lake": self.__getfilepath('aria-lake.png'),
+            "aurora-plateau": self.__getfilepath('aurora-plateau.png'),
+            "back": self.__getfilepath('back.png'),
+            "dispatch": self.__getfilepath('dispatch.png'),
+            "dungeons": self.__getfilepath('dungeons.png'),
+            "emulator-relative-reference": self.__getfilepath('emulator-relative-reference.png'),
+            "exit-stages-details": self.__getfilepath('exit-stages-details.png'),
+            "exp-bonus-stage": self.__getfilepath('exp-bonus-stage.png'),
+            "expedition": self.__getfilepath('expedition.png'),
+            "file-explorer": self.__getfilepath('file-explorer.jpg'),
+            "glacial-plains": self.__getfilepath('glacial-plains.png'),
+            "lunar-valley": self.__getfilepath('lunar-valley.png'),
+            "magma-crags": self.__getfilepath('magma-crags.png'),
+            "mirage-ruins": self.__getfilepath('mirage-ruins.png'),
+            "pagos-coast": self.__getfilepath('pagos-coast.png'),
+            "phantom-forest": self.__getfilepath('phantom-forest.png'),
+            "play": self.__getfilepath('play.png'),
+            "seabed-caves": self.__getfilepath('seabed-caves.png'),
+            "shady-shop": self.__getfilepath('shady-shop.png'),
+            "skip-dialogue": self.__getfilepath('skip-dialogue.png'),
+            "sky-falls": self.__getfilepath('sky-falls.png'),
+            "slumbering-city": self.__getfilepath('slumbering-city.png'),
+            "star-sanctuary": self.__getfilepath('star-sanctuary.png'),
+            "tower-of-chaos": self.__getfilepath('tower-of-chaos.png'),
+        }
+        try:
+            print(filename.upper())
+            x, y = pyautogui.locateCenterOnScreen(image_dictionary.get(filename), confidence=image_confidence, minSearchTime=timeoutInSeconds)
+            if event == 'double_click':
+                return pyautogui.doubleClick(x, y, interval=2)
+            if event == 'click':
+                return pyautogui.click(x, y)
+            if event == 'locate':
+                return (x, y)
+        except pyautogui.ImageNotFoundException as infe:
+            print('%s is not present in the game.' % filename.upper())
+            if throw_error == True:
+                raise infe
+            
+    def skipdialouge(self):
+        """
+            Continuously attempt to skip dialogue until error.
+        """
+        # If using to brand new device, necessary to press skip.
+        try:
+            while True:
+                
+                self.navigateto('skip-dialogue', True)
+        except:
+            print('Passing Skip dialouge error, no more skips found.')
+            pass
+    
+    def __getfilepath(self, filename:str):
+        index = -1
+        rootpath = self.getrootpath()
+        try:
+            index = self.__dir.index(filename)
+        except ValueError:
+            error = 'Filename doesn\'t exists in %s' % rootpath
+            print(error)
+        if index < 0:
+            return rootpath
+        
+        return '/'.join((*self.__images_path, self.__dir[index]))
+
+def main():
+    mslac = MSLAutomationCore()
+    # [GO TO ADVENTURE]
+    # mslac.navigateto('play')
+    # mslac.skipdialouge()
+
+    first_region_stages = ('phantom-forest', 'lunar-valley', 'aria-lake', 'mirage-ruins')
+    second_region_stages = ('pagos-coast', 'seabed-caves', 'magma-crags', 'star-sanctuary')
+    third_region_stages = ('sky-falls', 'slumbering-city', 'glacial-plains', 'aurora-plateau')
+
+    for stage in first_region_stages:
+        mslac.navigateto('play', event='double_click')
+        # mslac.skipdialouge()
+        mslac.navigateto(stage)
+        mslac.navigateto('exit-stages-details')
+        mslac.navigateto('back',image_confidence=0.7)
+
+    # [REGION NAVIGATION]
+    x, y = mslac.navigateto('emulator-relative-reference', event='locate')
+
+    # [SECOND REGION]
+    for stage in second_region_stages:
+        mslac.navigateto('play', event='double_click')
+        # mslac.skipdialouge()
+        pyautogui.moveTo(x+600,y+200)
+        pyautogui.dragTo(x, None, duration=.5, tween=pyautogui.easeInOutQuad)
+        mslac.navigateto(stage)
+        mslac.navigateto('exit-stages-details')
+        mslac.navigateto('back',image_confidence=0.7)
+
+    # [THIRD REGION]
+    for stage in third_region_stages:
+        mslac.navigateto('play', event='double_click')
+        # mslac.skipdialouge()
+        pyautogui.moveTo(x+800,y+200)
+        pyautogui.dragTo(x-525, None, duration=.5, tween=pyautogui.easeInOutQuad)
+        mslac.navigateto(stage)
+        mslac.navigateto('exit-stages-details')
+        mslac.navigateto('back',image_confidence=0.7)
+
+if __name__ == "__main__":
+    main()
